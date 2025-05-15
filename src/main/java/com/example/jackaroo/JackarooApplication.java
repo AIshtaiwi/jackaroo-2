6d45235e8adc92e13c2f17850699a62c843f1ade
AIshtaiwi <ish.ayham@gmail.com>
2025-05-15 00:37:54 -0400

    first commit

---
+++ src/main/java/com/example/jackaroo/JackarooApplication.java
@@ -0,0 +1,13 @@
+package com.example.jackaroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackarooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackarooApplication.class, args);
+	}
+
+}
